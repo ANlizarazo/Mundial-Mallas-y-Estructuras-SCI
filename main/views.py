ae5740104489991d760c806266fa3bb6cb6b959a
ANlizarazo <110872596+ANlizarazo@users.noreply.github.com>
2022-12-03 23:25:06 -0500

    ....

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render 
 
 def login(request):
-    context={}
+    nombre="Futuro Tecnologico"
+    context={
+        'nombres':nombre
+    }
     return render(request,'index.html',context)
